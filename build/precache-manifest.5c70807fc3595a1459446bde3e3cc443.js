79cd6a8497c537cfa7817e91f350304d03d74fe3
Eeva Sasaki <eeva.sasaki@relexsolutions.com>
2020-07-04 13:38:49 +0300

    Preparing 3.10

---
+++ build/precache-manifest.5c70807fc3595a1459446bde3e3cc443.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a531606a83ed03963cd5d5242be8859",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3f3e06a3b72f1823b51",
+    "url": "/static/css/main.eb82856b.chunk.css"
+  },
+  {
+    "revision": "c164db0d716c10a5f408",
+    "url": "/static/js/2.f7f5c33f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f7f5c33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3f3e06a3b72f1823b51",
+    "url": "/static/js/main.515f669c.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
